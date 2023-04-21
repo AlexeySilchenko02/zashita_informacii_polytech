4904559d2ba4a8bc9dd4104e15efd87a4922bcbe
_Allex_ <91027382+AlexeySilchenko02@users.noreply.github.com>
2023-04-21 17:20:55 +0300

    final version lr1

--- lr1/lr1_new_final.java
+++ lr1/lr1_new_final.java
@@ -1,5 +1,13 @@
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) throws IOException {
+        //переменнаые для записи хеш-кода двух файлов
         String hash1;
         String hash2;
         Boolean a = true;
@@ -22,6 +30,7 @@ public class Main {
 
         System.out.println("Хеш-код файла: " + hash1);
 
+        //ГЕНЕРАЦИЯ ФАЙЛА
         // Имя файла, который будем генерировать
         String newFileName = "newFile.txt";
 
@@ -88,10 +97,26 @@ public class Main {
         }
         else {
             int i = 0;
+            String fileName = "hash2.txt";
+            String hash3 = "";
+            FileWriter fileWriter = new FileWriter(fileName);  // создаем FileWriter
+
             while (a){
                 //удаление файла
                 File file = new File("newFile.txt");
                 file.delete();
+
+                Path filePath = Paths.get(fileName);
+                if (Files.exists(filePath)) // проверка существования файла
+                {
+                    fileWriter = new FileWriter(filePath.toString(), true); // открытие файла в режиме дозаписи
+                    fileWriter.write(hash3);  // записываем первую строку
+                    fileWriter.write("\n");   // записываем символ перевода строки
+                    fileWriter.close(); // закрытие файла
+                    //System.out.println("The line has been added to the file.");
+                } else {
+                    System.out.println("File does not exist.");
+                }
 
                 // Имя файла, который будем генерировать
                 newFileName = "newFile.txt";
@@ -112,7 +137,7 @@ public class Main {
                             // Перебираем все символы в строке, заменяем пробел на управляющий символ пробел с вероятностью 50%
                             for (char ch : line.toCharArray()) {
                                 if (ch == ' ') {
-                                    if (Math.random() < 0.5) {
+                                    if (Math.random() < 0.75) {
                                         newLine.append(' ');
                                     } else {
                                         newLine.append('\u200B'); // Управляющий символ пробел
@@ -151,14 +176,33 @@ public class Main {
                 hashArray = output.split("\\s+");
                 hash2 = hashArray[hashArray.length - 1];
 
-                System.out.println("Хеш-код файла 2: " + hash2);
+                //System.out.println("Хеш-код файла 2: " + hash2);
 
+                //ПРОВЕРКА СУЩЕСТВУЕТ ЛИ СГЕНИРИРОВАННЫЙ ХЕШ В ФАЙЛЕ
+                try {
+                    BufferedReader reader = new BufferedReader(new FileReader(fileName));
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        if (line.contains(hash2)) {
+                            System.out.println("Строка найдена: " + line);
+                            break;
+                        }
+                    }
+                    reader.close();
+                } catch (IOException e) {
+                    System.out.println("Ошибка при работе с файлом: " + e.getMessage());
+                }
+                
                 if(hash1 == hash2){
                     a = false;
                     System.out.println("Хеш-коды одинаковые! И кол-во попыток = " + i);
                 }
+                else if(hash2 == hash3){
+                    System.out.println("Коллизия хешкодов созданных файлов. Хешкод = " + hash3);
+                }
                 else {
                     i++;
+                    hash3 = hash2;
                     if(i > 50){
                         a = false;
                     }
